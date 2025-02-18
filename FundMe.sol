09dfa97f87ce47416644b75e7ea268777bf12145
nifeesleman <nsr-1868-14@amu.edu.et>
2025-02-18 16:11:40 +0300

    how revert work ?

--- FundMe.sol
+++ FundMe.sol
@@ -2,10 +2,16 @@
 pragma solidity ^0.8.26;
 
 contract FundMe{
+
+    uint256 public myValue=1;
     function fund() public payable {
 
-
+      //Allow user to send ether through minimem ether
+      //How do we send ether
+       myValue+=2;
        require( msg.value > 1e18 ,"Did send enough fund");
+       // what is revert
+    
 
     }
 }
