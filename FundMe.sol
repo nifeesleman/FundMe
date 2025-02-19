fb8d7cf0a8c2a1daae2da7339ee0e95fd14109db
nifeesleman <nsr-1868-14@amu.edu.et>
2025-02-19 12:03:34 +0300

    To get The Price And Conversation Rate

--- FundMe.sol
+++ FundMe.sol
@@ -3,15 +3,17 @@ pragma solidity ^0.8.26;
 
 contract FundMe{
 
-    uint256 public myValue=1;
+    uint256 public minimumUsd=5;
     function fund() public payable {
 
       //Allow user to send ether through minimem ether
       //How do we send ether
-       myValue+=2;
-       require( msg.value > 1e18 ,"Did send enough fund");
+      
+       require( msg.value >= minimumUsd ,"Did't send enough fund");
        // what is revert
     
 
     }
+    function getPrice() public {}
+    function getConverstionRate() public {}
 }
