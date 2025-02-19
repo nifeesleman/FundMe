db1288ee2d61b127316c2732d159f419493ec34e
nifeesleman <nsr-1868-14@amu.edu.et>
2025-02-19 16:20:16 +0300

    Adding Library

--- FundMe.sol
+++ FundMe.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-
+import {priceConverter} from "./priceConverter.sol";
 
 contract FundMe{
-
+   using priceConverter for uint256;
     uint256 public minimumUsd=5e18;
+
+
 
     address [] public funders; 
     mapping (address funder => uint256 amountFunded) public addressToAmountFunder;
@@ -23,19 +24,5 @@ contract FundMe{
        addressToAmountFunder[msg.sender]+=msg.value;
      
     }
-    function getPrice() public view returns(uint256) {
-       AggregatorV3Interface priceFeed= AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-       (, int256 answer,,,)= priceFeed.latestRoundData();
-
-       return uint256(answer*1e10);
-    }
-    function getConverstionRate(uint256 ethAmount) public view returns(uint256)  {
-      uint256 ethPrice =getPrice();
-      uint256 ethAmountInUsd=(ethPrice*ethAmount)/1e18;
-      return ethAmountInUsd;
-    }
-    //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-    function getVersion() public  view returns (uint256) {
-      return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-    }
+   
 }
