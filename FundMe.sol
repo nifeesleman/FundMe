c2e00124ffda4144bb327d210436e3720899179f
nifeesleman <nsr-1868-14@amu.edu.et>
2025-02-20 12:36:29 +0300

    Version 2

--- FundMe.sol
+++ FundMe.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.26;
 
 import {priceConverter} from "./priceConverter.sol";
-
+error NotOwner();
 contract FundMe{
    using priceConverter for uint256;
     uint256 public minimumUsd=5e18;
@@ -11,7 +11,10 @@ contract FundMe{
 
     address [] public funders; 
     mapping (address funder => uint256 amountFunded) public addressToAmountFunder;
-    
+    address public owner;
+    constructor(){
+      owner = msg.sender;
+    }
    function fund() public payable {
 
       //Allow user to send ether through minimem ether
@@ -24,11 +27,28 @@ contract FundMe{
        addressToAmountFunder[msg.sender]+=msg.value;
      
     }
-   function withdraw() public {
+   function withdraw() public onlyOwner {
+   
     for(uint256 funderIndex= 0;funderIndex<funders.length;funderIndex++){
       address funder =funders[funderIndex];
       addressToAmountFunder[funder]=0;
     }
     funders =new address[](0);
+    //call
+    (bool callSuccess,)=payable(msg.sender).call{value: address(this).balance}("");
+    require(callSuccess,"call failed");
+    revert();
    }
+   modifier onlyOwner(){
+    //  require(msg.sender == owner,"Must be owner!");
+    if(msg.sender != owner){revert NotOwner();}
+     _;
+   }
+
+   receive() external payable {
+    fund();
+    }
+    fallback() external payable {
+    fund();
+     }
 }
